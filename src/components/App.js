cc316a1076aa1afb5d4c5aff1d77c00677589ec2
Oluwaseun Akogun <seun407@gmail.com>
2019-10-30 17:11:34 +0100

    created VideoDetail component and added iframes to each VideoItem component

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("nba highlights");
+  }
 
   onTermSubmit = async term => {
     const KEY = "AIzaSyDXa4FE1HDIgPSWD5DGcBW9kbuQtzih2_M";
@@ -16,15 +21,33 @@ class App extends React.Component {
         q: term
       }
     });
-    this.setState({ videos: response.data.items });
-    console.log(this.state.videos);
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
+  };
+
+  onVideoSelect = video => {
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./VideoItem.css";
 
-const VideoItem = ({ video }) => {
-  const { thumbnails, title } = video.snippet;
+const VideoItem = ({ video, onVideoSelect }) => {
+  const { thumbnails, title, description } = video.snippet;
   return (
-    <div className="video-item item">
-      <img className="ui image" src={thumbnails.medium.url} alt="" />
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
+      <img className="ui image" src={thumbnails.medium.url} alt={description} />
       <div className="content">
         <div className="header">{title}</div>
       </div>
